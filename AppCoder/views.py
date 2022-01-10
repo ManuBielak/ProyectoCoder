32ad7369bf6a9a3b078e55dfb37189351ca8fa3a
Manuel Bielak <manuelbielak@hotmail.com>
2022-01-09 22:28:40 -0300

    Segundo commit

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
-
-from AppCoder.models import Curso
+from django.shortcuts import render, HttpResponse
 from django.http import HttpResponse
+from AppCoder.models import Curso
 
 # Create your views here.
 
-def crea_curso(self):
+def curso(request):
 
-    curso = Curso(nombre="Javascript", camada=112233)
+      curso =  Curso(nombre="Desarrollo web", camada="19881")
+      curso.save()
+      documentoDeTexto = f"--->Curso: {curso.nombre}   Camada: {curso.camada}"
 
-    curso.save()
 
-    return HttpResponse(f"Se creo el curso de {curso.nombre} con el nro de camada {curso.camada}")
+      return HttpResponse(documentoDeTexto)
+
+
+def inicio(request):
+
+      return render(request, "AppCoder/inicio.html")
+
+def cursos(request):
+
+      return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+
+      return render(request, "AppCoder/profesores.html")
+
+
+def estudiantes(request):
+
+      return render(request, "AppCoder/estudiantes.html")
+
+
+def entregables(request):
+
+      return render(request, "AppCoder/entregables.html")
